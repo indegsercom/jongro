bbb9c726d75871a6e67ef469e58ea6237b0c2e90
indegser <indegser@gmail.com>
2020-01-27 21:57:43 +0900

    fix typo on decrpy script

--- scripts/decrypt_secret.sh
+++ scripts/decrypt_secret.sh
@@ -4,4 +4,4 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" \
---output $PWD/secrets/mfirebase-admin-account.json $PWD/secrets/mfirebase-admin-account.json.gpg
+--output $PWD/secrets/mfirebase-admin-account.json $PWD/secrets/firebase-admin-account.json.gpg
